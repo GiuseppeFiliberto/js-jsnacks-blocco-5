bd2e23e46358f3b0afde60371742cbe27d8515ab
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-26 16:36:14 +0100

    add solution snack 5

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -1,4 +1,18 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
+const oddNumbs = nums.filter((number) =>{
+    if (number % 2 === 0){
+        return true
+    }
+    return false
+
+})
+console.log(oddNumbs);
+
+//  Qui si potrebbe usare anche una versione più corta 
+
+/* const oddNumbs = nums.filter((number) => number % 2 === 0)
+console.log(oddNumbs); */
+
 // Risultato: [2, 8, 4, 12]
