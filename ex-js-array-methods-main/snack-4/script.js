36325d2e617bd2016ff8030b746ff11790608e92
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-26 16:20:51 +0100

    add solution snack 4

--- ex-js-array-methods-main/snack-4/script.js
+++ ex-js-array-methods-main/snack-4/script.js
@@ -5,5 +5,10 @@ const posts = [
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
 ]
 
+const authorsPost = posts.map((name) => {
+  return name.author
+})
+console.log(authorsPost);
+
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
