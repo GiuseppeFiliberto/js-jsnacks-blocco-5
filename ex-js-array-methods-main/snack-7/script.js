6d4fe42a56284b2ae49bbd502edddadc1e4c3d04
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-26 16:45:47 +0100

    add solution

--- ex-js-array-methods-main/snack-7/script.js
+++ ex-js-array-methods-main/snack-7/script.js
@@ -20,4 +20,11 @@ const students = [
 ];
 
 // Recupera i dati dello studente con id 2
+
+const studentDetails = students.find((element) => {
+  return element.id === 2
+})
+
+console.log(studentDetails);
+
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
